327489c308d8f81f20787bb108a36d1e9ac80543
matthijsvz <30950994+matthijsvz@users.noreply.github.com>
2018-01-19 12:30:26 +0100

    Add files via upload

---
+++ opdracht 4/index_backup.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo MG1A Webformulieren</title>
+</head>
+<body>
+
+<form action="readform.php" method="post">
+    <input type="text" name="woonplaats" placeholder="woonplaats" />
+    //<input type="submit" name="submit" value="klik hier!" />
+//</form>
+
+//<form action="readform.php" method="post">
+    <input type="text" name="naam" placeholder="naam" />
+    //<input type="submit" name="submit" value="klik hier!" />
+//</form>
+
+//<form action="readform.php" method="post">
+    <input type="text" name="leeftijd" placeholder="leeftijd" />
+    <input type="submit" name="submit" value="klik hier!" />
+</form>
+
+</body>
+</html>
---
+++ opdracht 4/readform.php
@@ -0,0 +1,9 @@
+<?php
+$woonplaats = $_POST['woonplaats'];
+echo '<h1>Jij woont in ' . $woonplaats . '</h1>';
+
+$naam = $_POST['naam'];
+echo '<h1>Jouw naam is ' . $naam . '</h1>';
+
+$leeftijd = $_POST['leeftijd'];
+echo '<h1>Jouw naam is ' . $leeftijd . '</h1>';
