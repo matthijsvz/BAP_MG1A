1994781a53d7739c2900930d3d0bccd84ef72667
matthijsvz <30950994+matthijsvz@users.noreply.github.com>
2018-01-26 09:38:41 +0100

    Add files via upload

---
+++ opdracht3/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>opdracht3</title>
+</head>
+<body>
+<?php
+$one = "1";
+$two = "2";
+$three = "3";
+$four = "4";
+$five = "5";
+echo "1 t/m 5: $one, $two, $three, $four en $five";
+?>
+<br>
+<?php
+$antwoord = 5;
+if ( $antwoord == 5 )
+{
+    echo "Hoi";
+}
+?>
+<br>
+<?php
+$txt1 = "String";
+$txt2 = " Constants!";
+echo $txt1 . $txt2;
+?>
+</body>
+</html>
